2a53d22c77258fecafa3929df2baa7100c61c079
Hojat Fathpour <fathpour.hojat@gmail.com>
2025-03-06 16:36:43 +0330

    commit

--- component/exchanges/coinserver.ts
+++ component/exchanges/coinserver.ts
@@ -1,19 +1,22 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 import { HttpsProxyAgent } from "https-proxy-agent";
 import symbols from "../../symbols/symbols";
+import { CoinexOrderBookResponse, OrderBook, ResponseData } from "../extypes/types";
 
 const agent = new HttpsProxyAgent("http://127.0.0.1:10808");
 
-const coinBaseUrl: string = "https://api.coinex.com/v2";
+const coinBaseUrl: URL = new URL("https://api.coinex.com/v2/");
 
 const coinInstance = axios.create({
-  baseURL: coinBaseUrl,
+  baseURL: coinBaseUrl.toString(),
   httpsAgent: agent,
 });
 
-async function httpGetCoinexOrderBook(symbol) {
-  // const response = await coinInstance.get("/spot/depth");
-  const response = await coinInstance.get("/spot/depth", {
+
+async function httpGetCoinexOrderBook(
+  symbol: [string, string]
+): Promise<{ [key: string]: OrderBook }> {
+  const response: AxiosResponse<ResponseData> = await coinInstance.get("/spot/depth", {
     params: {
       market: symbol[1],
       limit: 5,
@@ -40,22 +43,39 @@ async function httpGetCoinexOrderBook(symbol) {
   return coinexOrderBooks;
 }
 
-function sortCoinexOrderBooks(data) {
-  const ask = data.depth.asks[0];
-  const bid = data.depth.bids[0];
-  return  {
-    [data.market]: { ask,bid },
-  }
+function sortCoinexOrderBooks(data: CoinexOrderBookResponse): {
+  [key: string]: OrderBook;
+} {
+  const ask: number[] = data.depth.asks[0];
+  const bid: number[] = data.depth.bids[0];
+  return {
+    [data.market]: { ask, bid },
+  };
 }
 
-function httpGetCoinexOrderBooks() {
-  const sortedCoinexOrderBooks = symbols.nobCoin.map(async function (symbol) {
-    return await httpGetCoinexOrderBook(symbol);
-  });
-  console.log("sortedCoinexOrderBooks:",sortedCoinexOrderBooks );
-  
+async function httpGetCoinexOrderBooks() {
+  const sortedCoinexOrderBooks: Promise<{ [key: string]: OrderBook }>[] =
+    symbols.nobCoin.map(async function (symbol: [string, string]) {
+      return httpGetCoinexOrderBook(symbol);
+    });
+
+  // const allOrderBooks = await Promise.allSettled(sortedCoinexOrderBooks);
+  // console.log("allOrderBooks",allOrderBooks);
+    
+  // sortedCoinexOrderBooks.forEach(async function (coinexob) {
+  //   try {
+  //     const ob = await coinexob
+  //     console.log("ob : ",ob);
+
+  //   } catch (error) {console.log("err");
+  //   }
+  // });
+
+  // console.log("sortedCoinexOrderBooks:", sortedCoinexOrderBooks);
 }
-httpGetCoinexOrderBooks()
+
+httpGetCoinexOrderBooks();
+
 export = {
   httpGetCoinexOrderBooks,
 };
---
+++ component/extypes/types.ts
@@ -0,0 +1,35 @@
+export interface OrderBook {
+  ask: number[];
+  bid: number[];
+}
+
+export interface CoinexOrderBookResponse {
+  depth: {
+    asks: number[][];
+    bids: number[][];
+  };
+  market: string;
+}
+
+export interface ResponseData {
+  data: CoinexOrderBookResponse;
+}
+
+// interface ResponseData {
+//     data : CoinexOrderBookResponse
+// }
+
+// interface ResponseDataData extends Partial<AxiosResponse> {
+//   data : ResponseData
+// }
+// response.data.data = {
+// depth: {
+//   asks: [ [fee,vol], [fee,vol], [fee,vol], [fee,vol], [fee,vol] ],
+//   bids: [ [fee,vol], [fee,vol], [fee,vol], [fee,vol], [fee,vol] ],
+//   checksum: 2262935607,
+//   last: '89427.12',
+//   updated_at: 1741182214097
+// },
+// is_full: true,
+// market: 'BTCUSDT'
+// }
